daad0af05c88dab7536ddff2e1eb612582c375a3
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-06-29 02:15:57 +0530

    Updates

---
+++ precache-manifest.4fcb05ac5db7a6925b82f5acac9bda91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55d46bf517f4982785142018a9d11e8",
+    "url": "/Dropdown-Menu-React/index.html"
+  },
+  {
+    "revision": "b9539368f3974d0a394a",
+    "url": "/Dropdown-Menu-React/static/css/main.f6726b0e.chunk.css"
+  },
+  {
+    "revision": "0cc26cff2219372c2b00",
+    "url": "/Dropdown-Menu-React/static/js/2.9085d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dropdown-Menu-React/static/js/2.9085d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9539368f3974d0a394a",
+    "url": "/Dropdown-Menu-React/static/js/main.070fbfd2.chunk.js"
+  },
+  {
+    "revision": "bf04e7b15aaaa83513bb",
+    "url": "/Dropdown-Menu-React/static/js/runtime-main.d7214603.js"
+  }
+]);
